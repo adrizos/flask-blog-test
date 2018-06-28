af794c83e1da1e065cc67f620b28366451f0a5ce
Alex Drizos <adrizosimac@Alexs-iMac.local>
2018-06-28 17:12:27 -0400

    adding pages

--- app/flaskblog.py
+++ app/flaskblog.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+
+posts = [
+    {
+        'author': 'Alex Drizos',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 26, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'June 27, 2018'
+    }
+]
+
+testTitle = "Test Tile"
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page!</h1>"
+    return render_template('home.html', posts=posts, title=testTitle)
 
 
 @app.route("/about")
 def about():
-    return "About Page"
+    return render_template('about.html')
