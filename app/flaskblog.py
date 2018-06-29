1146478ccff4eeb3dc4260bb99133ca50ee6c752
Alex Drizos <adrizosimac@Alexs-iMac.local>
2018-06-28 22:17:37 -0400

    adding register and login html pages

--- app/flaskblog.py
+++ app/flaskblog.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '708f3de3453f99ed0ebfd0db0dd899a2'
@@ -30,3 +32,14 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
