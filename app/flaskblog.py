610a801eebd8a87c4e96909c54bd5c1bd9e02580
Alex Drizos <adrizosimac@Alexs-iMac.fios-router.home>
2018-06-30 16:26:54 -0400

    db file doesnt appear after command line creation

--- app/flaskblog.py
+++ app/flaskblog.py
@@ -2,11 +2,34 @@
 
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '708f3de3453f99ed0ebfd0db0dd899a2'
+app.config['SQLAlCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False) #max 20 characters
+    email = db.Column(db.String(120), unique=True, nullable=False) #max 120 characters
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg') #will hash image to something unique , will add default image later
+    password = db.Column(db.String(60), nullable=False) #password will be hashed to 60 chars
+    posts = db.relationship('Post', backref='author', lazy=True) #connects from posts model according to author, referencing Post class not a post
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow) #not using utcnow() because we dont want time at that moment
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False) #id of user who authored the post
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+#dummy data
 posts = [
     {
         'author': 'Alex Drizos',
